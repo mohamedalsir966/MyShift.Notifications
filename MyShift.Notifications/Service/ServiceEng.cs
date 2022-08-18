c18144dfa063bb884fc95f6c5bb24e931e4ef690
Mohamed Alsir <mohamed.alsir@cxunicorn.com>
2022-08-18 20:29:52 +0400

    farist

---
+++ MyShift.Notifications/Service/ServiceEng.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using MyShift.Notifications.Entitys;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShift.Notifications.Service
+{
+    public class ServiceEng : IService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogsRepository _LogsRepository;
+        public ServiceEng(IConfiguration configuration, ILogsRepository logsRepository)
+        {
+            _configuration = configuration;
+            _LogsRepository = logsRepository;
+        }
+        public async Task<string> GetDataToNotifiy()
+        {
+            var DataTobeNotifyed = await _LogsRepository.GetLogsQueries();
+
+            var UpdateData= await _LogsRepository.UpdateLogsCommand(DataTobeNotifyed);
+
+            var QueueMessage = JsonConvert.SerializeObject(DataTobeNotifyed);
+            return QueueMessage;
+        }
+        public bool SendDataToQueue(string message)
+        {
+            try
+            {
+                string connectionString = _configuration.GetValue<string>("AzureWebJobsStorage");
+                QueueClient queueClient = new QueueClient
+                (connectionString, "test4");
+                queueClient.CreateIfNotExists();
+                if (queueClient.Exists())
+                {
+                    queueClient.SendMessage(Convert.ToBase64String(Encoding.UTF8.GetBytes(message)));
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+      
+    }
+}
---
+++ Persistence/Repositories/LogsRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class LogsRepository : ILogsRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public LogsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ShiftEntityLog>> GetLogsQueries()
+        {
+           var logs= await _context.ShiftEntityLog.Where(x=>x.status == "not sent").ToListAsync();
+            return logs;
+        }
+
+        public async Task <string> UpdateLogsCommand(List<ShiftEntityLog> items)
+        {
+            foreach (var item in items)
+            {
+                item.status = "sent";
+                await _context.SaveChangesAsync();
+            }
+            return "Data Updated";
+        }
+    }
+}
